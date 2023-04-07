75c1b97804620b579ad641ef006d0c10a9ed74dc
Walid Bouladam <bouladam.walid86@gmail.com>
2023-04-08 10:19:27 +1200

    Fix: define correct base URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bigwaldor.github.io',
-  base: '/eportfolio-v2/e-portfolio',
+  base: '/e-portfolio/',
 });
