262bd606e88366fc7ff9af00e43c1a8396d3e29e
Walid Bouladam <bouladam.walid86@gmail.com>
2023-05-30 19:10:51 +1200

    Configure partytown config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,14 @@
 import { defineConfig } from 'astro/config';
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bigwaldor.github.io',
+  integrations: [
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
 });
